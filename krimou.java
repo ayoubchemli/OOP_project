917efb172211c9ef5b45d90b17c2b1e92eb2e4af
ayoub <chemliayoub414@gmail.com>
2024-03-09 11:01:32 +0100

    seguemna krimou

---
+++ krimou.java
@@ -0,0 +1,18 @@
+public class krimou {
+
+    public static void main(String[] args) {
+        // Testing code can go here
+        System.out.println("Testing Java class");
+        
+        // Example of calling a method
+        int result = addNumbers(5, 10);
+        System.out.println("Result of adding numbers: " + result);
+
+        System.out.println("kremo khoya");
+    }
+    
+    // Example method
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
