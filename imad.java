6d5ca7e7309d7cd88cd69b085e593d210c09ef5d
ayoub <chemliayoub414@gmail.com>
2024-03-09 11:08:27 +0100

    imaaaad

---
+++ imad.java
@@ -0,0 +1,21 @@
+public class imad {
+    
+    public static void main(String[] args) {
+        // Testing code can go here
+        System.out.println("Testing Java class");
+        
+        // Example of calling a method
+        int result = addNumbers(5, 10);
+        System.out.println("Result of adding numbers: " + result);
+
+        System.out.println("imad akhina");
+        
+        System.out.println("hh");
+    }
+    
+    // Example method
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
+
